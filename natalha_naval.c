2e309843af5a8317df1141d89f37984ed45b0231
renatosoa12-code <renatosoa12@gmail.com>
2025-11-29 21:05:07 -0300

    Update natalha_naval.c
    os varcos estao representados por "0"
    o mar esta representado por "~"
    foi ajustado a posição dos numeros e letras nos cantos do tabuleiro

--- natalha_naval.c
+++ natalha_naval.c
@@ -2,59 +2,50 @@
 
 int main() {
 
-    int matriz[11][11] = { 0 };
+    char campo[10][10];
 
-    // Cabeçalho superior (1 a 10)
-    for (int c = 1; c <= 10; c++) {
-        matriz[0][c] = c;
+    // Preenche o campo com '~'
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            campo[i][j] = '~';
+        }
     }
 
-    // Cabeçalho lateral (A a J)
-    for (int l = 1; l <= 10; l++) {
-        matriz[l][0] = 'A' + (l - 1);
+    // ===== POSICIONAMENTO DOS BARCOS =====
+
+    // Barco horizontal de tamanho 2 (linha 2, colunas B e C)
+    campo[1][1] = '0';
+    campo[1][2] = '0';
+
+    // Barco vertical de tamanho 3 (coluna F, linhas 5,6,7)
+    campo[4][5] = '0';
+    campo[5][5] = '0';
+    campo[6][5] = '0';
+
+    // Barco diagonal tamanho 3 (diagonal principal)
+    campo[2][7] = '0';
+    campo[3][8] = '0';
+    campo[4][9] = '0';
+
+    // Barco diagonal tamanho 3 (diagonal invertida)
+    campo[8][2] = '0';
+    campo[7][3] = '0';
+    campo[6][4] = '0';
+
+    // ===== IMPRESSÃO DO TABULEIRO =====
+
+    // Letras no topo
+    printf("   ");
+    for (int j = 0; j < 10; j++) {
+        printf("%c ", 'A' + j);
     }
+    printf("\n");
 
-    // ============================
-    // POSICIONAMENTO DOS NAVIOS
-    // ============================
-
-    // 1) Navio VERTICAL (tamanho 2) em B3 (linhas 2 e 3, coluna 3)
-    matriz[2][3] = 3;
-    matriz[3][3] = 3;
-
-    // 2) Navio HORIZONTAL (tamanho 3) em F5 (linha 6, colunas 5,6,7)
-    matriz[6][5] = 3;
-    matriz[6][6] = 3;
-    matriz[6][7] = 3;
-
-    // 3) Navio DIAGONAL 1 (tamanho 3) começando em H2
-    // posições: H2, I3, J4
-    matriz[8][2] = 3;
-    matriz[9][3] = 3;
-    matriz[10][4] = 3;
-
-    // 4) Navio DIAGONAL 2 (tamanho 3) começando em C8
-    // posições: C8, D9, E10
-    matriz[3][8] = 3;
-    matriz[4][9] = 3;
-    matriz[5][10] = 3;
-
-    // ============================
-    // IMPRESSÃO DA TABELA
-    // ============================
-
-    for (int l = 0; l < 11; l++) {
-        for (int c = 0; c < 11; c++) {
-
-            if (l == 0 && c == 0)
-                printf("    ");
-            else if (l == 0)
-                printf("%4d", matriz[l][c]);
-            else if (c == 0)
-                printf("%4c", matriz[l][c]);
-            else
-                printf("%4d", matriz[l][c]);
-
+    // Números laterais + conteúdo da matriz
+    for (int i = 0; i < 10; i++) {
+        printf("%2d ", i + 1);
+        for (int j = 0; j < 10; j++) {
+            printf("%c ", campo[i][j]);
         }
         printf("\n");
     }
